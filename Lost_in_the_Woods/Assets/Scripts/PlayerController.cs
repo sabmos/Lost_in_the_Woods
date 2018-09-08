3c265bb4baa298378b85757592a3df16718ce92c
Sam <23609502+sabmos@users.noreply.github.com>
2018-09-08 18:13:02 -0500

    Basic Functionality
    Added all basic functionality. NavMesh movement, terrain details, menus, game logic scripts, sounds, trees

---
+++ Lost_in_the_Woods/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject destination;
+    public NavMeshAgent agent;
+
+    // I am only using this for testing
+    private void Start()
+    {
+        Vector3 destinationVector3 = destination.transform.position;
+        agent.SetDestination(destinationVector3);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+    }
+
+    // Move our argent
+    public void MoveTo(GameObject destinationObject) {
+        Vector3 destinationVector3 = destinationObject.transform.position;
+        agent.SetDestination(destinationVector3);
+    }
+}
---
+++ Lost_in_the_Woods/Assets/Scripts/TopOfHillTrigger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopOfHillTrigger : MonoBehaviour {
+
+    public GameObject decisionMenu;
+    public AudioSource decisionMenuAudioSource;
+
+    private float secondsToWait = 3f;
+    private bool wolfGrowlPlayed = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Call our DogGrowl IEnumerator
+        StartCoroutine(WolfGrowl());
+    }
+
+    // Plays a dog growling sound after the user enters the collider trigger
+    // at the top of the hill.
+    public IEnumerator WolfGrowl () {
+
+        // Wait for a few seconds, and then play a dog growl sound
+        yield return new WaitForSeconds(secondsToWait);
+        GameObject wolfSoundObject = GameObject.Find("Wolf_Growl");
+        AudioSource wolfSound = wolfSoundObject.GetComponent<AudioSource>();
+        if (!wolfGrowlPlayed) {
+            wolfSound.Play();
+            Debug.Log("dog growl played");
+            wolfGrowlPlayed = true;
+            StartCoroutine(OpenDecisionsMenu());
+        }
+    }
+
+    // Opens the decisions menu after a few seconds
+    public IEnumerator OpenDecisionsMenu() {
+
+        // Wait a few more seconds, then display a decision menu
+        yield return new WaitForSecondsRealtime(secondsToWait);
+        decisionMenu.SetActive(true);
+        decisionMenuAudioSource.Play();
+    }
+}
